560ccb11b03409d557e3996f98130ad0372f17fa
bacor <mail@bascornelissen.nl>
2020-04-03 19:03:59 +0200

    add missing transformation plots

--- update.py
+++ update.py
@@ -4,14 +4,6 @@ from catafolk.dataset import Dataset
 
 if __name__ == '__main__':
     # TODO add command line args?
-    # parser = argparse.ArgumentParser(description='Index all datasets.')
-    # parser.add_argument('index_dir', type='str')
-    # parser.add_argument('data_dir', type='str')
-        
-    # cur_dir = os.path.dirname(__file__)
-    # root_dir = os.path.abspath(os.path.join(cur_dir, os.path.pardir))
-    # datasets_dir = os.path.join(root_dir, 'datasets')
-
     datasets = [
         'bronson-child-ballads',
         'creighton-nova-scotia',
@@ -25,7 +17,7 @@ if __name__ == '__main__':
         'essen-china-han',
         'essen-china-natmin',
         'essen-china-shanxi',
-        'essen-deutschl-erk'
+        'essen-deutschl-erk',
         'essen-deutschl-boehme',
         'essen-deutschl-altdeu1'
     ]
@@ -33,9 +25,5 @@ if __name__ == '__main__':
         print(dataset_id)
         dataset = Dataset(dataset_id)
         dataset.make()
-        dataset.plot_transformations()
-        # d = get_dataset(dataset_id, datasets_dir)
-    #     d.save_index()
-    #     d.save_properties()
-    
+        # dataset.plot_transformations()
     
