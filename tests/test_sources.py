919ad6fff6f99a8af1fce31375991cd8cde59947
bacor <mail@bascornelissen.nl>
2020-04-10 14:30:20 +0200

    finish and document file source

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -13,19 +13,19 @@ CUR_DIR = os.path.dirname(__file__)
 TEST_DATASETS_DIR = os.path.join(CUR_DIR, 'datasets')
 
 class TestSource(unittest.TestCase):
-    def test_iterable_source(self):
-        entries = [ {'id': i, 'foo': 'bar'} for i in range(10)]
-        source = IterableSource(entries, name='test')
-        df = source.collect()
+    def test_source(self):
+        entries = [ {'id': i*10, 'foo': 'bar'} for i in range(10)]
+        source = Source(entries, name='test')
+        df = source.data
         self.assertEqual(len(df), 10)
-        self.assertEqual(len(df.columns), 1)
+        self.assertEqual(len(df.columns), 2)
         self.assertTrue((df['foo'] == 'bar').all())
     
     def test_csv_source(self):
         path = os.path.join(CUR_DIR, 'test_csv_source.csv')
         source = CSVSource(path, name='csv', id_field='item_id')
-        df = source.collect()
-        self.assertEqual(df.index.name, 'id')
+        df = source.data
+        self.assertEqual(df.index.name, 'cf_id')
         self.assertEqual(len(df), 10)
         self.assertEqual(df.columns[0], 'item_id')
         self.assertEqual(df.columns[1], 'col1')
@@ -36,9 +36,10 @@ class TestSource(unittest.TestCase):
         transformations = [
             ['format', 'item_id', 'id', {'pattern': 'item-{:0>3}'}]
         ]
-        source = IterableSource(entries, name='test', id_field='item_id',
-            id_transformations=transformations)
-        df = source.collect()
+        id_transformer = Transformer(transformations)
+        source = Source(entries, name='test', 
+            id_transformer=id_transformer)
+        df = source.data
         self.assertEqual(df.index[0], 'item-000')
         self.assertEqual(df.index[1], 'item-001')
         self.assertEqual(df.index[2], 'item-002')
@@ -46,7 +47,15 @@ class TestSource(unittest.TestCase):
         self.assertEqual(len(df.columns), 2)
         self.assertEqual(df.loc['item-000', 'foo'], 'bar')
 
+    def test_file_source_missing_id(self):
+        data_dir = os.path.join(TEST_DATASETS_DIR, 'bronson-child-ballads', 'data')
+        source = FileSource(data_dir, '*.krn', name='file', id_field='id')
+        with self.assertRaises(ValueError):
+            source.data
+
     def test_file_source(self):
         data_dir = os.path.join(TEST_DATASETS_DIR, 'bronson-child-ballads', 'data')
         source = FileSource(data_dir, '*.krn', name='file')
-        print(source)
+        df = source.data
+        self.assertEqual(len(df), 9)
+        self.assertTrue('ONM' in df.columns)
